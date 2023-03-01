c4407b37132bf6a1cddd9579f2af15e799e58696
rhymaado <mightydhiyamatra@yahoo.co.id>
2023-03-01 14:52:16 +0700

    create get request

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// URL = PROTOCOL://HOST:PORT/ENDPOINT
+// PROTOCOL = http, https
+// HOST = ip, domain
+// PORT = ketika ip ada port
+// ENDPOINT = alat navigasi
+app.get("/", (req, res) => {
+  //   res.json({
+  //     msg: "Selamat Datang di Toko Kopi API",
+  //   });
+  const path = require("path");
+  res.status(201).sendFile(path.join(__dirname, "/src/html/welcome.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
   //   console.log("Welcome");
