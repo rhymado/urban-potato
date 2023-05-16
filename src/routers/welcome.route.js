eff48093bd714113c51ed49ef2a2824435a2431c
rhymaado <mightydhiyamatra@yahoo.co.id>
2023-05-16 12:51:00 +0700

    setup firebase

--- src/routers/welcome.route.js
+++ src/routers/welcome.route.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const welcomeController = require("../controllers/welcome.controller");
 const memoryUpload = require("../middlewares/memoryUpload");
 // const { checkToken } = require("../middlewares/auth");
+const notification = require("../utils/notification");
 
 const welcomeRouter = Router();
 
@@ -10,5 +11,24 @@ const welcomeRouter = Router();
 welcomeRouter.get("/", welcomeController.welcomePage);
 
 welcomeRouter.post("/", memoryUpload.single("image"), welcomeController.cloudUpload);
+
+welcomeRouter.post("/notification", (req, res) => {
+  const { body } = req;
+  // ambil token FCM
+  console.log(body.token);
+  notification
+    .send(body.token, {
+      title: "Remote",
+      body: "From Backend",
+    })
+    .then(() =>
+      res.status(200).json({
+        msg: "OK",
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = welcomeRouter;
---
+++ src/utils/notification.js
@@ -0,0 +1,22 @@
+const { Messaging } = require("firebase-admin/messaging");
+const app = require("../configs/firebase");
+
+const Notification = new Messaging(app);
+
+const send = async (token, notification) => {
+  try {
+    const message = {
+      token,
+      notification,
+    };
+    await Notification.send(message);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+module.exports = {
+  send,
+};
